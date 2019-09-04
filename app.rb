bda693bb38aeb9162607e5eb1d57a47c2372783b
EdZeno <eduard.kulcsickij.gut@gmail.com>
2019-09-04 16:03:47 +0100

    players submit their names

--- app.rb
+++ app.rb
@@ -2,9 +2,19 @@ require 'sinatra'
 
 class Battle < Sinatra::Base
 
-get '/' do
-'Testing infrastructure working?'
-end
+  get '/' do
+    'Testing infrastructure working?'
+  end
 
-run! if app_file == $0
+  get '/sign_in' do
+    erb :index
+  end
+
+  post '/names' do
+    @player1=params[:name_player1]
+    @player2=params[:name_player2]
+    erb :play
+  end
+
+  run! if app_file == $0
 end
