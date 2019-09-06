796773aae29b3163ec03737df7e991d109a46628
Student <student@Makerss-MacBook-Pro-7.local>
2019-09-06 15:13:23 +0100

    test hit point reduction

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,14 @@
 class Player
 
-  attr_reader :name
+  attr_reader :name, :hp
 
-  def initialize(name = 'Boris')
+  def initialize(name = 'Boris', hp = 60)
     @name = name
+    @hp = hp
+  end
+
+  def damage
+    @hp -= 10
   end
 
 end
